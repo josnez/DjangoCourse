8e3011c85330fa6548545c30d96bc4fa6e6b1564
Josue Nuñez Prada <josuenunezprada@gmail.com>
2022-02-12 14:12:54 -0500

    views

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("", views.index, name="index")
+  path("", views.index, name="index"),
+  #Exam: /polls/5/
+  path("<int:question_id>/", views.index, name="index"),
+  #Exam: /polls/5/results
+  path("<int:question_id/results>/", views.index, name="index"),
+  #Exam: /polls/5/vote
+  path("<int:question_id/vote>/", views.index, name="index"),
 ]
